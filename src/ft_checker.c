2a85142d64f3ccc5c2fd1401b685447ed13e453d
Rizky Ario <rizkyario@yahoo.com>
2018-03-12 03:21:18 +0100

    Added pw_is_line_valid

--- src/ft_checker.c
+++ src/ft_checker.c
@@ -6,11 +6,38 @@
 /*   By: rnugroho <rnugroho@students.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 14:30:36 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/03/12 01:51:53 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/03/12 03:21:05 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int
+	pw_is_line_valid(char *line)
+{
+	if (ft_strnequ(line, "sa", 3))
+		return (1);
+	else if (ft_strnequ(line, "sb", 3))
+		return (1);
+	else if (ft_strnequ(line, "pa", 3))
+		return (1);
+	else if (ft_strnequ(line, "pb", 3))
+		return (1);
+	else if (ft_strnequ(line, "ra", 3))
+		return (1);
+	else if (ft_strnequ(line, "rb", 3))
+		return (1);
+	else if (ft_strnequ(line, "rr", 3))
+		return (1);
+	else if (ft_strnequ(line, "rra", 4))
+		return (1);
+	else if (ft_strnequ(line, "rrb", 4))
+		return (1);
+	else if (ft_strnequ(line, "rrr", 4))
+		return (1);
+	else
+		return (0);
+}	
 
 void
 	pw_checker(char *line, t_array *a, t_array *b)
@@ -37,7 +64,7 @@ void
 		(((void (*)())op[i])(b, a));
 }
 
-void
+static int
 	pw_check_line(t_array *a, t_array *b)
 {
 	int		i;
@@ -46,6 +73,11 @@ void
 	i = 0;
 	while (get_next_line(0, &line))
 	{
+		if (!pw_is_line_valid(line))
+		{
+			ft_dprintf(2, "Error\n");
+			return (-1);
+		}
 		pw_checker(line, a, b);
 		if (g_isverbose)
 		{
@@ -57,6 +89,7 @@ void
 	}
 	if (g_istotal)
 		ft_printfln("Total steps: %i", i);
+	return (0);
 }
 
 int
@@ -73,7 +106,12 @@ int
 		i = pw_getoptions(av);
 		if (pw_get_arg(&a, i, ac, av) == -1)
 			return (0);
-		pw_check_line(&a, &b);
+		if (pw_check_line(&a, &b) == -1)
+		{
+			fta_clear(&a);
+			fta_clear(&b);
+			return (0);
+		}
 		if (b.size == 0 && pw_is_sorted(&a))
 			ft_printfln("OK");
 		else
